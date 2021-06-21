4d766beb26382e466b596aeed4a6d4ffeefc1f66
Evgeny Kulinich <ekulinich85@gmail.com>
2021-06-21 15:10:58 +0300

    changed mysql_sync.sh rewrite without ssh

--- mysql_sync.sh
+++ mysql_sync.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-server1='10.150.4.239'
-server2='10.150.4.240'
-user_db1=root
-user_db2=root
-passwd_db1=1
-passwd_db2=1
-db1=test1
-db2=test2
 
-ssh vagrant@$server1 "mysqldump -u$user_db1 -p$passwd_db1 $db1 > ~/test1dump.sql"
-scp -3 vagrant@$server1:~/test1dump.sql vagrant@$server2:~
-ssh vagrant@$server2 "mysqldump -u$user_db2 -p$passwd_db2 $db2 > ~/test2dump.sql && mysql -u$user_db2 -p$passwd_db2 $db2 < ~/test1dump.sql"
+#Mysql production
+
+production_host='10.150.4.239'
+production_login=root
+production_pass=1
+prod_db=test1
+
+#Mysql stage
+
+stage_host='10.150.4.240'
+stage_login=root
+stage_pass=1
+stage_db=test2
+
+mysqldump -u$production_login -h $production_host -p$production_pass $prod_db > ~/prod_dump.sql
+mysqldump -u$stage_login -p$stage_pass $stage_db > ~/stage_dump.sql && mysql -u$stage_login -p$stage_pass $stage_db < ~/prod_dump.sql
