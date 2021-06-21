7572434dc89abaca45b604168e4e1f2d0b86b247
Evgeny Kulinich <ekulinich85@gmail.com>
2021-06-21 13:55:36 +0300

    changed s3backet_sync.sh from cp to sync v2

--- s3backet_sync.sh
+++ s3backet_sync.sh
@@ -3,5 +3,5 @@
 s3backetfrom=testbacket1spawn
 s3backetto=testbacket2spawn
 
-aws s3 sync s3://$s3backetfrom/ s3://$s3backetto/ --recursive --exclude "*" --include "*.png"
+aws s3 sync s3://$s3backetfrom/ s3://$s3backetto/ --exclude "*" --include "*.png"
 
